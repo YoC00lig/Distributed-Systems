85a1ec9c010b5bca664d3ec173be8fe10aec7d38
YoC00lig <kulig.j@vp.pl>
2024-05-04 19:21:53 +0200

    Update gen_proto.sh

--- homework4-5/grpc/server/gen_proto.sh
+++ homework4-5/grpc/server/gen_proto.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# I truly detest python import system
-# https://github.com/protocolbuffers/protobuf/issues/1491#issuecomment-1463981985
-
 mkdir -p ./generated
 printf "import sys\nfrom pathlib import Path\nsys.path.append(str(Path(__file__).parent))" > ./generated/__init__.py
 python3 -m grpc_tools.protoc \
